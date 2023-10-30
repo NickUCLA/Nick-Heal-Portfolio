b02d409aa573468b17abe5f32c8e290fc14f670a
NickUCLA <133281012+NickUCLA@users.noreply.github.com>
2023-10-30 16:54:40 -0700

    Adding mobile navbar feature

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import windiConfig from "./windi.config.mjs"; // also consider renaming the file for clarity
+import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-
-// Dynamically import the WindiCSS plugin
-const loadWindiCSSPlugin = async () => {
-  const windiCSSModule = await import("vite-plugin-windicss");
-  return windiCSSModule.default;
-};
+import postcssImport from "postcss-import";
 
 // Configure Vite
-const config = async () => {
-  const windiCSSPlugin = await loadWindiCSSPlugin();
-
+const config = () => {
   return defineConfig({
-    plugins: [
-      react(),
-      windiCSSPlugin({
-        config: windiConfig,
-        // ... other WindiCSS plugin options
-      }),
-    ],
+    plugins: [react()],
     css: {
       postcss: {
-        plugins: [autoprefixer],
+        plugins: [postcssImport, tailwindcss, autoprefixer],
       },
     },
     server: {
